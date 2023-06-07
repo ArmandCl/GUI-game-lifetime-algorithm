5fabe589d32f5000294b586973de8042320fa5ff
RayanBT <btrayan21@gmail.com>
2023-06-07 16:54:55 +0200

    Écriture des méthodes de la classe GridPaneScenario.java

--- SAE_202_IHM/src/main/java/vue/GridPaneScenario.java
+++ SAE_202_IHM/src/main/java/vue/GridPaneScenario.java
@@ -1,7 +1,13 @@
 package vue;
 
-import javafx.scene.control.ComboBox;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.scene.control.*;
 import javafx.scene.layout.GridPane;
+
+import static modele.ConstantesScenario.NUM_SCENARIO;
 
 /**
  * La classe GridPaneScenario est une sous-classe de GridPane qui représente la grille de sélection du scénario et
@@ -9,12 +15,65 @@ import javafx.scene.layout.GridPane;
  */
 public class GridPaneScenario extends GridPane {
 
+    private ComboBox chNumScenario;
+    private ToggleGroup chRadioGroup;
+
     /**
      * Constructeur de la classe GridPaneScenario.
      * Initialise les éléments de la grille de sélection du scénario et de la solution.
      */
     public GridPaneScenario() {
+        setHgap(20);
+        setVgap(10);
+        setPadding(new Insets(0, 0, 0, 0));
+        int ligne = 0;
 
+        Label titre = new Label("Module de selection");
+        GridPane.setHalignment(titre, HPos.CENTER);
+        this.add(titre,0,ligne++,6,1);
+
+        Separator sepateurDate = new Separator();
+        sepateurDate.setPadding(new Insets(10,0,5,0));
+        this.add(sepateurDate,0,ligne++,6,1);
+
+        Label labNiv = new Label("Choisir un scenario:");
+        this.add(labNiv,0,ligne,1,1);
+        chNumScenario =peupleComboBox(NUM_SCENARIO);
+        this.add(chNumScenario,2,ligne++);
+
+        Label labSol = new Label("Choisir une solution:");
+        this.add(labSol,0,ligne,1,1);
+        chRadioGroup = new ToggleGroup();
+
+        RadioButton radioButtonDebutant = new RadioButton("Efficace");
+        radioButtonDebutant.setUserData("Efficace");
+        radioButtonDebutant.setToggleGroup(chRadioGroup);
+        this.add(radioButtonDebutant,1,ligne,2,1);
+
+        RadioButton radioButtonMoyen = new RadioButton("Exhaustive");
+        radioButtonMoyen.setUserData("Exhaustive");
+        radioButtonMoyen.setToggleGroup(chRadioGroup);
+        radioButtonMoyen.setMnemonicParsing(true);
+        this.add(radioButtonMoyen,3,ligne++,4,1);
+
+        Separator sepateurBouton = new Separator();
+        sepateurBouton.setPadding(new Insets(10,0,5,0));
+        this.add(sepateurBouton,0,ligne++,6,1);
+
+        Button buttonAnnuler = new Button("Annuler");
+        this.add(buttonAnnuler,2,ligne,2,1);
+        Button buttonValider = new Button("Valider");
+        this.add(buttonValider,4,ligne);
+
+        buttonValider.setOnAction(HBoxRoot.getChControleur());
+        chNumScenario.setOnAction(HBoxRoot.getChControleur());
+        buttonAnnuler.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                reset();
+            }
+        });
+        reset();
     }
 
     /**
@@ -24,14 +83,19 @@ public class GridPaneScenario extends GridPane {
      * @return la ComboBox peuplée avec les éléments du tableau
      */
     private ComboBox<Integer> peupleComboBox(Integer[] nbre) {
-
+        ComboBox <Integer> comboBox = new ComboBox<>();
+        for (Integer nbre_scen:nbre){
+            comboBox.getItems().add(nbre_scen);
+        }
+        return comboBox;
     }
 
     /**
      * Réinitialise les valeurs de sélection du scénario et de la solution.
      */
     public void reset() {
-
+        chNumScenario.setValue(NUM_SCENARIO[0]);
+        chRadioGroup.getToggles().get(0).setSelected(true);
     }
 
     /**
@@ -40,7 +104,7 @@ public class GridPaneScenario extends GridPane {
      * @return le numéro du scénario sélectionné
      */
     public int getNumScenario() {
-
+        return (int) chNumScenario.getValue();
     }
 
     /**
@@ -49,6 +113,8 @@ public class GridPaneScenario extends GridPane {
      * @return la solution sélectionnée ("Efficace" ou "Exhaustive")
      */
     public String getSolution() {
-
+        RadioButton selectedRadioButton = (RadioButton) chRadioGroup.getSelectedToggle();
+        String toogleGroupValue = selectedRadioButton.getText();
+        return toogleGroupValue;
     }
 }
