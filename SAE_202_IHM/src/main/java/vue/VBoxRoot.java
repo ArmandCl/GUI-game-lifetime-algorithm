97339866179c7b44d508f4322be08f863d2dcc7c
RayanBT <btrayan21@gmail.com>
2023-06-07 16:08:01 +0200

    Conception des méthodes de la classe VBoxRoot.java

--- SAE_202_IHM/src/main/java/vue/VBoxRoot.java
+++ SAE_202_IHM/src/main/java/vue/VBoxRoot.java
@@ -1,6 +1,39 @@
 package vue;
 
+import controleur.Controleur;
 import javafx.scene.layout.VBox;
 
+/**
+ * La classe VBoxRoot est une sous-classe de VBox qui représente la racine de l'interface graphique de l'application RPG.
+ * Elle contient une instance de HBoxRoot et de MenuDeroulant, ainsi qu'un objet Controleur pour la gestion des événements.
+ */
 public class VBoxRoot extends VBox {
+
+
+    /**
+     * Constructeur de la classe VBoxRoot.
+     * Crée une instance de Controleur, HBoxRoot et MenuDeroulant.
+     * Ajoute le MenuBar de MenuDeroulant et HBoxRoot à la VBoxRoot.
+     */
+    public VBoxRoot() {
+
+       }
+
+    /**
+     * Obtient l'objet Controleur utilisé dans l'application.
+     *
+     * @return l'objet Controleur
+     */
+    public static Controleur getChControleur() {
+
+    }
+
+    /**
+     * Obtient l'objet MenuDeroulant utilisé dans l'application.
+     *
+     * @return l'objet MenuDeroulant
+     */
+    public static MenuDeroulant getChMenuDeroulant() {
+
+    }
 }
