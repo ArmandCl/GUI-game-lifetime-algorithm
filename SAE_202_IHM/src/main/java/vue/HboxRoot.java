33ddedd4e4b52ef3bc02acc3775b24d401f29304
RayanBT <btrayan21@gmail.com>
2023-06-07 16:16:47 +0200

    Conception des méthodes de la classe HboxRoot.java

--- SAE_202_IHM/src/main/java/vue/HboxRoot.java
+++ SAE_202_IHM/src/main/java/vue/HboxRoot.java
@@ -1,6 +1,74 @@
 package vue;
 
+import controleur.Controleur;
 import javafx.scene.layout.HBox;
+import modele.Joueur;
+import outil.LectureFichierTexte;
 
+/**
+ * La classe HBoxRoot est une sous-classe de HBox qui représente la racine de l'interface graphique de l'application RPG.
+ * Elle contient plusieurs éléments graphiques, tels que VBoxAffichageScenario, GridPaneScenario et GridPaneAffichageSolution.
+ */
 public class HboxRoot extends HBox {
+    /**
+     * Constructeur de la classe HBoxRoot.
+     * Initialise les différents éléments graphiques utilisés dans l'interface et les ajoute à la HBoxRoot.
+     */
+    public HBoxRoot() {
+
+    }
+
+    /**
+     * Obtient l'objet Controleur utilisé dans l'application.
+     *
+     * @return l'objet Controleur
+     */
+    public static Controleur getChControleur() {
+
+    }
+
+    /**
+     * Obtient l'objet VBoxAffichageScenario utilisé dans l'application.
+     *
+     * @return l'objet VBoxAffichageScenario
+     */
+    public static VBoxAffichageScenario getChAfficheScenario() {
+
+    }
+
+    /**
+     * Obtient l'objet GridPaneAffichageSolution utilisé dans l'application.
+     *
+     * @return l'objet GridPaneAffichageSolution
+     */
+    public static GridPaneAffichageSolution getChAffichageSolution() {
+
+    }
+
+    /**
+     * Obtient l'objet GridPaneScenario utilisé dans l'application.
+     *
+     * @return l'objet GridPaneScenario
+     */
+    public static GridPaneScenario getChGridPaneScenario() {
+
+    }
+
+    /**
+     * Obtient l'objet LectureFichierTexte utilisé dans l'application.
+     *
+     * @return l'objet LectureFichierTexte
+     */
+    public static LectureFichierTexte getLectureFichierTexte() {
+
+    }
+
+    /**
+     * Obtient l'objet Joueur utilisé dans l'application.
+     *
+     * @return l'objet Joueur
+     */
+    public static Joueur getChJoueur() {
+
+    }
 }
