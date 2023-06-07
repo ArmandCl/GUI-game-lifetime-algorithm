fed1b9f5628640de176049128d2f6f2de0e20d9f
RayanBT <btrayan21@gmail.com>
2023-06-07 15:53:08 +0200

    Création des constantes qui nous seront utile pour l'IHM

---
+++ SAE_202_IHM/src/main/java/modele/ConstantesFichierScenario.java
@@ -0,0 +1,11 @@
+package modele;
+
+public interface ConstantesFichierScenario {
+
+    final String[] NOM_FICHIER = {"scenario_0.txt","scenario_1.txt","scenario_2.txt","scenario_3.txt","scenario_4.txt",
+            "scenario_5.txt","scenario_6.txt","scenario_7.txt","scenario_8.txt","scenario_9.txt","scenario_10.txt"};
+    final String[] NOM_SCENARIO ={"Scenario 0","Scenario 1","Scenario 2","Scenario 3","Scenario 4",
+            "Scenario 5","Scenario 6","Scenario 7","Scenario 8","Scenario_9","Scenario 10"};
+
+
+}
---
+++ SAE_202_IHM/src/main/java/modele/ConstantesScenario.java
@@ -0,0 +1,5 @@
+package modele;
+
+public interface ConstantesScenario {
+    final Integer[] NUM_SCENARIO = {0,1,2,3,4,5,6,7,8,9,10};
+}
