798cfeca7e956e3693c138366bcec82641498a8a
RayanBT <btrayan21@gmail.com>
2023-06-07 16:13:34 +0200

    Conception des méthodes de la classe MenuDeroulant.java

--- SAE_202_IHM/src/main/java/vue/MenuDeroulant.java
+++ SAE_202_IHM/src/main/java/vue/MenuDeroulant.java
@@ -1,4 +1,67 @@
 package vue;
 
+import javafx.scene.control.Alert;
+import javafx.scene.control.MenuButton;
+
+/**
+ * La classe MenuDeroulant représente un menu déroulant personnalisé pour les scénarios dans l'application RPG.
+ * Elle crée un MenuButton contenant des MenuItem pour chaque scénario, et permet de sélectionner un scénario.
+ */
 public class MenuDeroulant {
+
+    /**
+     * Crée un MenuButton personnalisé contenant les MenuItem pour chaque scénario.
+     *
+     * @return le MenuButton créé
+     */
+    private MenuButton createMenuBar() {
+
+    }
+
+    /**
+     * Sélectionne un scénario en attribuant le numéro du scénario passé en paramètre à chNumScenario.
+     *
+     * @param parNumScenario le numéro du scénario sélectionné
+     * @return le numéro du scénario sélectionné
+     */
+
+    public int chNumScenarioSelect(int parNumScenario){
+
+    }
+
+    /**
+     * Obtient le numéro du scénario sélectionné.
+     *
+     * @return le numéro du scénario sélectionné
+     */
+    public int getChNumScenario(){
+
+    }
+
+    /**
+     * Constructeur de la classe MenuDeroulant.
+     * Crée la barre de menu en appelant la méthode createMenuBar.
+     */
+    public MenuDeroulant() {
+
+    }
+
+    /**
+     * Obtient la barre de menu créée.
+     *
+     * @return la barre de menu
+     */
+    public MenuButton getMenuBar() {
+
+    }
+
+    /**
+     * Affiche une fenêtre d'alerte.
+     * Utile pour debug quand on clique sur un menuItem
+     */
+    private void alert(){
+
+    }
 }
+
+
