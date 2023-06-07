7f876452af06c5b0d09531e4ca98041305774502
RayanBT <btrayan21@gmail.com>
2023-06-07 16:59:36 +0200

    Création et écriture des méthodes de la classe Controleur.java

--- SAE_202_IHM/src/main/java/controleur/Controleur.java
+++ SAE_202_IHM/src/main/java/controleur/Controleur.java
@@ -1,4 +1,64 @@
 package controleur;
 
-public class Controleur {
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.MenuItem;
+import modele.*;
+import outil.LectureFichierTexte;
+import vue.*;
+
+import java.util.ArrayList;
+
+public class Controleur implements EventHandler {
+
+
+    /**
+     * Gère les événements déclenchés par les interactions avec les éléments graphiques.
+     *
+     * @param event l'événement à traiter
+     */
+    @Override
+    public void handle(Event event) {
+        VBoxAffichageScenario affichageScenario = HBoxRoot.getChAfficheScenario();
+        GridPaneAffichageSolution affichageSolution = HBoxRoot.getChAffichageSolution();
+        LectureFichierTexte fichierScenario = HBoxRoot.getLectureFichierTexte();
+        GridPaneScenario scenarioPane = HBoxRoot.getChGridPaneScenario();
+        SolutionEfficace solutionEfficace;
+        SolutionExhaustive solutionExhaustive;
+        Joueur joueur = HBoxRoot.getChJoueur();
+        MenuDeroulant menuDeroulant = VBoxRoot.getChMenuDeroulant();
+
+        if (event.getSource() instanceof Button){
+            int numScenario = scenarioPane.getNumScenario();
+            Scenario scenario = fichierScenario.getScenario(numScenario);
+            ArrayList<Quete> quetes = fichierScenario.getKey(numScenario);
+            affichageScenario.update(numScenario,quetes);
+            if (scenarioPane.getSolution()=="Efficace") {
+                joueur.reset();
+                solutionEfficace = new SolutionEfficace(scenario, joueur);
+                solutionEfficace.solutionGloutonne();
+                affichageSolution.update(joueur, joueur.getChQuetesCompleteesGloutonne2());
+            }else {
+                joueur.reset();
+                solutionExhaustive = new SolutionExhaustive(scenario, joueur);
+                solutionExhaustive.solutionExhaustive();
+                affichageSolution.update(joueur, joueur.getChQuetesCompleteesExhaustive2());
+            }
+
+        }
+
+        if (event.getSource() instanceof ComboBox<?>){
+            int numScenario = menuDeroulant.getChNumScenario();
+            ArrayList<Quete> quetes = fichierScenario.getKey(numScenario);
+            affichageScenario.update(numScenario,quetes);
+        }
+
+        if (event.getSource() instanceof MenuItem){
+            int numScenarioMenu = menuDeroulant.getChNumScenario();
+            ArrayList<Quete> quetes = fichierScenario.getKey(numScenarioMenu);
+            affichageScenario.update(numScenarioMenu,quetes);
+        }
+    }
 }
