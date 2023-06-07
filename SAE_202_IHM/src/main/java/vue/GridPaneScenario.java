ac480e98b7e49dd8c9b7b996075cc2ce8908f10a
RayanBT <btrayan21@gmail.com>
2023-06-07 16:25:20 +0200

    Conception des méthodes de la classe GridPaneScenario.java

--- SAE_202_IHM/src/main/java/vue/GridPaneScenario.java
+++ SAE_202_IHM/src/main/java/vue/GridPaneScenario.java
@@ -1,6 +1,54 @@
 package vue;
 
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.GridPane;
 
+/**
+ * La classe GridPaneScenario est une sous-classe de GridPane qui représente la grille de sélection du scénario et
+ * de la solution dans l'interface graphique.
+ */
 public class GridPaneScenario extends GridPane {
+
+    /**
+     * Constructeur de la classe GridPaneScenario.
+     * Initialise les éléments de la grille de sélection du scénario et de la solution.
+     */
+    public GridPaneScenario() {
+
+    }
+
+    /**
+     * Peuple une ComboBox avec les éléments d'un tableau d'entiers.
+     *
+     * @param nbre un tableau d'entiers
+     * @return la ComboBox peuplée avec les éléments du tableau
+     */
+    private ComboBox<Integer> peupleComboBox(Integer[] nbre) {
+
+    }
+
+    /**
+     * Réinitialise les valeurs de sélection du scénario et de la solution.
+     */
+    public void reset() {
+
+    }
+
+    /**
+     * Récupère le numéro du scénario sélectionné.
+     *
+     * @return le numéro du scénario sélectionné
+     */
+    public int getNumScenario() {
+
+    }
+
+    /**
+     * Récupère la solution sélectionnée.
+     *
+     * @return la solution sélectionnée ("Efficace" ou "Exhaustive")
+     */
+    public String getSolution() {
+
+    }
 }
