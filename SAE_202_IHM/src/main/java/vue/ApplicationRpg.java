d97cc1785bf44229126cb038377ddb37e8afd10b
RayanBT <btrayan21@gmail.com>
2023-06-07 16:04:38 +0200

    Conception des méthodes de la classe ApplicationRpg.java

--- SAE_202_IHM/src/main/java/vue/ApplicationRpg.java
+++ SAE_202_IHM/src/main/java/vue/ApplicationRpg.java
@@ -1,6 +1,33 @@
 package vue;
 
 import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+/**
+ * La classe ApplicationRpg est une application JavaFX qui lance l'analyseur RPG.
+ * Elle crée une fenêtre avec une scène contenant une racine VBoxRoot pour afficher l'interface graphique.
+ */
 public class ApplicationRpg extends Application {
+
+    /**
+     * La méthode start est appelée lors du démarrage de l'application.
+     * Elle crée une instance de VBoxRoot, crée une scène avec cette racine et l'affiche sur la fenêtre du stage.
+     *
+     * @param stage le stage de l'application JavaFX
+     */
+    public void start (Stage stage){
+
+
+    }
+
+    /**
+     * La méthode main est le point d'entrée de l'application.
+     * Elle lance l'application en appelant la méthode launch avec les arguments spécifiés.
+     *
+     * @param args les arguments de la ligne de commande
+     */
+    public static void main(String[] args) {
+
+    }
 }
