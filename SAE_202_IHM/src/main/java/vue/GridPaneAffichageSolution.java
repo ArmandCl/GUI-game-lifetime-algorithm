f16e4440e8a42b1b0ce268471fd8239f7f8ef1ca
RayanBT <btrayan21@gmail.com>
2023-06-07 16:33:37 +0200

    Conception des méthodes de la classe GridPaneAffichageSolution.java

--- SAE_202_IHM/src/main/java/vue/GridPaneAffichageSolution.java
+++ SAE_202_IHM/src/main/java/vue/GridPaneAffichageSolution.java
@@ -1,6 +1,34 @@
 package vue;
 
 import javafx.scene.layout.GridPane;
+import modele.Joueur;
+import modele.Quete;
 
+import java.util.ArrayList;
+
+/**
+ * Cette vue sera utilisée pour afficher le résultat de la solution choisie.
+ * La solution sera affichée sous forme de texte à droite de la page.
+ */
 public class GridPaneAffichageSolution extends GridPane {
+
+    /**
+     * Constructeur de la classe GridPaneAffichageSolution.
+     * Initialise les éléments pour afficher le résultat de la solution choisie.
+     *
+     * @param joueur le joueur contenant les résultats de la solution
+     */
+    public GridPaneAffichageSolution(Joueur joueur) {
+
+    }
+
+    /**
+     * Met à jour les éléments de la vue avec les nouvelles valeurs.
+     *
+     * @param joueur le joueur contenant les résultats de la solution
+     * @param parQuetesRealise la liste des quêtes réalisées
+     */
+    public void update(Joueur joueur, ArrayList<Quete> parQuetesRealise) {
+
+    }
 }
